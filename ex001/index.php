1af7f22e8715d77ad5e9c7a7e95f5ee745fca689
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-01-30 23:11:14 -0300

    first commit

---
+++ ex001/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dados do servidor</title>
+</head>
+<body>
+    <h1>
+        Dados do Servidor
+    </h1>
+    <?php
+        phpinfo();
+    ?>
+    
+</body>
+</html>
