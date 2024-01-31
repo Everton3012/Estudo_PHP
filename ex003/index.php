4cf468ac67edb025c8fdc8af4b4c7f41b50c434c
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-01-30 23:59:33 -0300

    variaveis e constates

---
+++ ex003/index.php
@@ -0,0 +1,17 @@
+<?php 
+    $nome = "Everton";
+    $sobrenome = "Moura";
+
+    const PAIS = "Brasil";
+
+    echo "Muito Prazer, $nome $sobrenome voce mora no " . PAIS;
+
+    $nome = " David";
+    $sobrenome = "Ribeiro";
+
+    echo "Muito Prazer, $nome $sobrenome";
+
+    echo "Muito Prazer, $nome $sobrenome voce mora no " . PAIS;
+
+
+?>
