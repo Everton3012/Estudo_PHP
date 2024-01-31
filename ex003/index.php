fc6d68f8ae1923c3470e996460a0b1fc6f30d7c6
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-01-31 09:48:28 -0300

    tipos

--- ex003/index.php
+++ ex003/index.php
@@ -1,8 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php 
+
+    $num = 0x1A;
     $nome = "Everton";
     $sobrenome = "Moura";
 
-    const PAIS = "Brasil";
+    echo $num;
+
+    const PAIS = "Brasil ";
 
     echo "Muito Prazer, $nome $sobrenome voce mora no " . PAIS;
 
@@ -13,5 +25,18 @@
 
     echo "Muito Prazer, $nome $sobrenome voce mora no " . PAIS;
 
+    $v = 300;
+    var_dump($v);
 
+    $vet = [6,3.5,"unreal",65, true];
+    var_dump($vet);
+
+    class Pessoa {
+        private string $nome;
+    }
+
+     $p = new Pessoa;
+     var_dump($p);
 ?>
+</body>
+</html>
