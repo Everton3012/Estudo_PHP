2d13dd0f5f8ef916e6445b5590855601aa676547
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-02-02 13:04:53 -0300

    somador de valores

---
+++ ex005/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>interação com formularios</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        // Capturando os dados do Formulário Retroalimentado
+        $valor1 = $_GET['v1'] ?? 0;
+        $valor2 = $_GET['v2'] ?? 0;
+
+    ?>
+    <main>
+        <h1>Somador de Valores</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="V1">Valor 1</label>
+            <input type="number" name="v1" id="v1" value="<?=$valor1?>" />
+            <label for="v2">Valor 2</label>
+            <input type="number" name="v2" id="v2" value="<?=$valor2?>" />
+            <input type="submit" value="Somar" />
+        </form>
+    </main>
+
+    <section>
+        <h2>Resultado da Soma</h2>
+        <?php 
+            $soma = $valor1 + $valor2;
+
+            print "<p>A soma entre os valores $valor1 e $valor2 é igual a $soma</p>"
+        ?>
+    </section>
+</body>
+</html>
